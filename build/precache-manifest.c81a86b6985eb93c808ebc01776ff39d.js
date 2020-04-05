dd2f87bca660e784bc92f8dec126e425b4651b38
orYoffe <ragiop07@gmail.com>
2020-04-05 09:54:23 +0200

    added build files

---
+++ build/precache-manifest.c81a86b6985eb93c808ebc01776ff39d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68df4f94bc68b71539c708f964b10ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291446afba4616cc3dd2",
+    "url": "/static/css/main.88243f1c.chunk.css"
+  },
+  {
+    "revision": "487e66ba068bd059ea4b",
+    "url": "/static/js/2.c8cf01e4.chunk.js"
+  },
+  {
+    "revision": "9045c73d641426a0ba6cf1435d370d83",
+    "url": "/static/js/2.c8cf01e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291446afba4616cc3dd2",
+    "url": "/static/js/main.6a908880.chunk.js"
+  },
+  {
+    "revision": "09eb2fae383ea5eae762",
+    "url": "/static/js/runtime-main.074d3413.js"
+  }
+]);
