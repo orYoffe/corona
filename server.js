4503b40847ad517908b0d095baf2f742583ee563
orYoffe <ragiop07@gmail.com>
2020-04-05 10:06:04 +0200

    uncommented api

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const compression = require('compression');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-// const Covid19 = require('./jsu');
+const Covid19 = require('./jsu');
+
+const covid19 = new Covid19();
 
 process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at:', p, 'reason:', reason);
@@ -15,36 +17,43 @@ const app = express();
 const port = process.env.PORT || 5000;
 const env = process.env.NODE_ENV || 'test';
 
-app.use(function (req, res, next) {
-  if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type',
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
-    return;
-  }
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type',
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+  });
+}
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(compression());
+
+app.post('/api/data', (req, res) => {
+  return Promise.all([
+    covid19.getData(),
+    covid19.geTimeSeriesData('confirmed'),
+  ]).then(([d, time]) => {
+    res.json({d, time});
+  });
+});
 
 app.use(function (req, res, next) {
   if (req.url.match(/^\/(css|js|img|font)\/.+/)) {
@@ -53,19 +62,9 @@ app.use(function (req, res, next) {
   next();
 });
 
-// const covid19 = new Covid19();
-// app.post('/api/data', (req, res) => {
-//   return Promise.all([
-//     covid19.getData(),
-//     covid19.geTimeSeriesData('confirmed'),
-//   ]).then(([d, time]) => {
-//     res.json({d, time});
-//   });
-// });
-
 app.use(express.static(path.resolve(__dirname, './build')));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
